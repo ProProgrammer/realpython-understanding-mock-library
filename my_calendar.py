0b1a4eac4028cc8ec7c8b546a5b9abd96b5f125c
Deep Sukhwani <deepsukhwani@gmail.com>
2019-09-18 17:55:05 +0530

    Using .configure_mock and Mock.__init__ to configure python Mock

--- my_calendar.py
+++ my_calendar.py
@@ -71,5 +71,29 @@ class TestCalendar(unittest.TestCase):
             get_holidays()
 
 
+class TestCalendarSideEffectOnMockWithIterable(unittest.TestCase):
+    def test_get_holidays_retry(self):
+        # Create a new Mock to initiate a Response
+        response_mock = Mock()
+        response_mock.status_code = 200
+        response_mock.json.return_value = {
+            '12/25': 'Christmas',
+            '7/4': 'Independence Day'
+        }
+
+        # Set the side effect of .get()
+        requests.get.side_effect = [Timeout, response_mock]
+
+        # Test that the first request raises a Timeout
+        with self.assertRaises(Timeout):
+            get_holidays()
+
+        # Now retry expecting a successful response
+        assert get_holidays()['12/25'] == 'Christmas'
+
+        # Finally, assert .get() was called twice
+        assert requests.get.call_count == 2
+
+
 if __name__ == '__main__':
     unittest.main()
