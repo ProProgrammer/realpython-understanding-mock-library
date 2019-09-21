5c2e95ca8f66d1cbc053adcab155f24a73306e35
Deep Sukhwani <deepsukhwani@gmail.com>
2019-09-21 09:46:43 +0530

    Using patch as a context manager with python `with` statement
    - `mock_requests` param from test signature is removed since now the mock
    object is created using context manager inside the test function

--- tests.py
+++ tests.py
@@ -7,14 +7,14 @@ from my_calendar_patch_decorator import get_holidays
 
 
 class TestCalendar(unittest.TestCase):
-    @patch('my_calendar_patch_decorator.requests')
-    def test_get_holidays_timeout(self, mock_requests):
-        mock_requests.get.side_effect = Timeout
 
-        with self.assertRaises(Timeout):
-            get_holidays()
+    def test_get_holidays_timeout(self):
+        with patch('my_calendar_patch_decorator.requests') as mock_requests:
+            mock_requests.get.side_effect = Timeout
+            with self.assertRaises(Timeout):
+                get_holidays()
 
-        mock_requests.get.assert_called_once_with('http://localhost/api/holidays')
+            mock_requests.get.assert_called_once_with('http://localhost/api/holidays')
 
 
 if __name__ == '__main__':
