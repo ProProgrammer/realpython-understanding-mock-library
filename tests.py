66f78b3e67fac4048faf120449cf1aecc434d75d
Deep Sukhwani <deepsukhwani@gmail.com>
2019-09-21 09:55:20 +0530

    Patching an object's attribute - in this case its get attribute of requests object
    - mock_requests param is added to test function definition since the mocked
    object as a result of @patch.object() decorator is passed to test function
    
    - object() takes same config params that patch() does but instead of passing
    target's path, we provide the target object itself (requests in this case)
    as the first parameter. Second parameter is the attribute of the target
    object that we are trying to mock.
    
    - We could also use object() as a context manager just like patch()

--- tests.py
+++ tests.py
@@ -1,20 +1,15 @@
 import unittest
 from unittest.mock import patch
 
-from requests.exceptions import Timeout
-
-from my_calendar_patch_decorator import get_holidays
+from my_calendar_patch_decorator import requests, get_holidays
 
 
 class TestCalendar(unittest.TestCase):
 
-    def test_get_holidays_timeout(self):
-        with patch('my_calendar_patch_decorator.requests') as mock_requests:
-            mock_requests.get.side_effect = Timeout
-            with self.assertRaises(Timeout):
-                get_holidays()
-
-            mock_requests.get.assert_called_once_with('http://localhost/api/holidays')
+    @patch.object(requests, 'get', side_effect=requests.exceptions.Timeout)
+    def test_get_holidays_timeout(self, mock_requests):
+        with self.assertRaises(requests.exceptions.Timeout):
+            get_holidays()
 
 
 if __name__ == '__main__':
